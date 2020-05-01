555fdf8bed6349a32013ef83fbd1d0c4d02d5dcb
qqqlab <46283638+qqqlab@users.noreply.github.com>
2020-05-01 18:26:41 +0200

    .

--- qqq_aes.h
+++ qqq_aes.h
@@ -1,7 +1,7 @@
 #ifndef _QQQ_AES_H_
 #define _QQQ_AES_H_
 
-// Encrypt plain text state[16] to encrypted state[16], key[16] gets destroyed
+// Encrypt plain text state[16] to encrypted state[16], key[16] is unchanged
 void aes_encrypt(uint8_t* key, uint8_t* state);
 
 #endif // _QQQ_AES_H_
