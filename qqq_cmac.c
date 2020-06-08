4cda49b595ee6867e9e245fe4378b0134d11c8d7
qqqlab <46283638+qqqlab@users.noreply.github.com>
2020-06-08 10:55:41 +0200

    Update qqq_cmac.c

--- qqq_cmac.c
+++ qqq_cmac.c
@@ -3,30 +3,35 @@
 
 // Compute length prepended CBC-CMAC of msg[msglen] with AES128 key[16], result is in cmac[16]. Keeps key and msg intact.
 void cmac(uint8_t* key, uint8_t* msg, uint32_t msglen, uint8_t* cmac) {
+	uint8_t cpos = 0;
+	uint32_t mpos = 0;
+  
 	//clear cmac
 	memset(cmac, 0x00, 16);	
 	
-	//prepend 4 byte length little endian
-	cmac[0] = msglen;
-	cmac[1] = msglen >> 8;
-        cmac[2] = msglen >> 16;
-	cmac[3] = msglen >> 24;
-	
-	//load up to 12 bytes from msg and encrypt first block
-	uint16_t pos = 0;
-	while(pos<12 && pos<msglen) {
-		cmac[pos+4] = msg[pos];
-		pos++;
+	//prepend 1 or 5 bytes with lenght
+	if(msglen<255) {
+	  cmac[cpos++] = msglen;
+	}else{
+	  cmac[cpos++] = 0xff;
+    cmac[cpos++] = msglen;
+	  cmac[cpos++] = msglen >> 8;
+    cmac[cpos++] = msglen >> 16;
+	  cmac[cpos++] = msglen >> 24;
+  }
+  
+	//load up to 15 bytes from msg and encrypt first block
+	while(cpos<16 && mpos<msglen) {
+		cmac[cpos++] = msg[mpos++];
 	}
 	aes_encrypt(key, cmac);
 	
-	while(pos < msglen) {
-		//xor next block, then encrypt
-		for(uint8_t i=0;i<16;i++) {
-			if(pos >= msglen) break;
-			cmac[i] ^= msg[pos];
-			pos++;
-		}
+	while(mpos < msglen) {
+		//xor next (partial) block, then encrypt
+    cpos = 0;
+    while(cpos<16 && mpos<msglen) {
+		  cmac[cpos++] ^= msg[mpos++];
+	  }
 		aes_encrypt(key, cmac);
 	}
 }
